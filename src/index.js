cdcbb150073294b2d6ec2529380ca8a2d211da42
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-09-17 21:11:13 +0100

    getting the urls back to the console

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 console.log('%c HI', 'color: firebrick')
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+const imgContainer = document.getElementById("dog-image-container");
+
+document.addEventListener("DOMContentLoaded", function() {
+    fetch(imgUrl)
+        .then(function(response) {
+            return response.json();
+          // return response.blob();
+        })
+        .then(json => {
+           // for (let i=0; i<json['message'].length; i++ ) {}
+            console.log(json['message'])
+        })
+});
